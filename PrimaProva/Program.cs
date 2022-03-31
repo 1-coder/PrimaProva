faccc3939d4361618da6e8ab03bd2fa29c0a9494
Bryan <bryanlucchetta@gmail.com>
2022-03-31 12:37:03 +0200

    Add project files.

---
+++ PrimaProva/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.Write("Aggiungi un nome: ");
+
+var testo = Console.ReadLine();
+
+Console.WriteLine(testo);
+
+
